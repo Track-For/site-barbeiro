2ca4202ff5e1b799a2166ba5e75796f3f8e12721
Luiz Fhellippe <fhellippe04092001@gmail.com>
2026-08-22 10:56:09 -0300

    Enable Vue runtime template compiler

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,16 @@ export default defineConfig(async () => {
   const { cloudflare } = await import('@cloudflare/vite-plugin');
 
   return {
+    resolve: {
+      alias: {
+        vue: 'vue/dist/vue.esm-bundler.js',
+      },
+    },
+    define: {
+      __VUE_OPTIONS_API__: true,
+      __VUE_PROD_DEVTOOLS__: false,
+      __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
+    },
     plugins: [
       sites(),
       cloudflare({
